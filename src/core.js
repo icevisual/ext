9234ddeb079d64c8b187f5589f7cc6a8dd42ea1a
icevisual <icevisual@hotmail.com>
2020-03-19 17:53:23 +0800

    Finish v0

--- src/core.js
+++ src/core.js
@@ -26,6 +26,7 @@ define(['WS', 'Utils', 'Logger', 'Proto'], function(ws, Utils, Logger, Proto) {
 	}
 	// 获取 播放器 标签
 	var $video = document.querySelector(videoSeletor);
+
 	console.log("iqiyi video1=", $video);
 	// 获取当前播放的视频名字
 	var video_name = document.querySelector(".header-link").innerHTML;
@@ -35,24 +36,47 @@ define(['WS', 'Utils', 'Logger', 'Proto'], function(ws, Utils, Logger, Proto) {
 
 	var movie_start = false;
 	var script_start = false;
-	
+	var script_id = 0;
+	// 循环 监测 视频是否跳转
+	var Watcher_lastTime = 0;
+	var Watcher_loopInterval = 2000;
+
 	ws.OnMessage = function(data) {
-		if(data && data['code'] == 0 && data['cmd'] == 'VideoInfo') {
+		console.log("data", data);
+		data = JSON.parse(data)
+
+		if(data['cmd'].toString().toLowerCase() == 'scriptjump') {
+			if(data['code'] == 102) {
+				if(script_id) {
+					var ptime = parseInt($video.currentTime * 1000);
+					Watcher_lastTime = ptime;
+					ws.SendMessage(Proto.Cmd_PlayScript(ptime, script_id));
+					script_start = true;
+				} else {
+					console.log("Jump Failed", "Script Not Played");
+					script_start = false;
+				}
+			}
+
+		}
+
+		if(data && data['code'] == 0 && data['cmd'] && data['cmd'].toString().toLowerCase() == 'videoinfo') {
 			console.log("Get Script And Play");
-			ws.SendMessage(Proto.Cmd_PlayScript($video.duration,1));
+			script_id = parseInt(data['msg']);
+			ws.SendMessage(Proto.Cmd_PlayScript(parseInt($video.currentTime * 1000), script_id));
 			script_start = true;
 		}
 	}
-	console.log("c1=",Proto.Cmd_VideoInfo(window.location.href, video_name, $video.duration))
-	console.log("c2=",Proto.Cmd_PlaySmell(1,2,3))
 	// 定时读取 视频播放器 时间
-	setInterval(function() {
+
+	// 更改 定时 同步
+	var _WaitVideoTimer = setInterval(function() {
 		if($video) {
 			if(!movie_start && $video.duration > 900) {
 				movie_start = true;
-				var c = Proto.Cmd_VideoInfo(window.location.href, video_name, $video.duration);
-				console.log("c=",c)
-				ws.SendMessage(c);
+				ws.SendMessage(Proto.Cmd_VideoInfo(window.location.href, video_name, parseInt($video.duration * 1000)));
+				
+				clearInterval(_WaitVideoTimer);
 				return;
 			}
 			if(movie_start && script_start) {
@@ -61,7 +85,20 @@ define(['WS', 'Utils', 'Logger', 'Proto'], function(ws, Utils, Logger, Proto) {
 			//console.log("ws.readyState = " + ws.readyState);
 			console.log("video pos=", video_name, $video.currentTime, "/", $video.duration);
 		}
-	}, 5000);
+	}, 10000);
+	// 每十次监测，强制加入 一次同步
+	var checkLoopTime = 1;
+	var _WatherTimer = setInterval(function() {
+		if($video && movie_start && script_start) {
+			var nowTime = parseInt($video.currentTime * 1000);
+			if(checkLoopTime % 10 == 0 || Math.abs(nowTime - Watcher_loopInterval - Watcher_lastTime) > 100) {
+				console.log("video Jump =", Watcher_lastTime, "/", parseInt($video.duration * 1000), "=>", nowTime, " / ", parseInt($video.duration * 1000));
+				ws.SendMessage(Proto.Cmd_ScriptJump(nowTime));
+			}
+			checkLoopTime ++;
+			Watcher_lastTime = nowTime;
+		}
+	}, Watcher_loopInterval);
 
 	return {
 		'ws': ws,
