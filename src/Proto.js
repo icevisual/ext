9df385f43f0bf0d286eb83c5b16a21fc74385e55
icevisual <icevisual@hotmail.com>
2020-03-19 16:00:41 +0800

    u

--- src/Proto.js
+++ src/Proto.js
@@ -48,29 +48,39 @@ define([], function() {
 			return JSON.stringify(result);
 		},
 		Cmd_VideoInfo: function(url, name, length) {
-			return this.AssembleProtoAuto(CMDConst.Cmd_VideoInfo, this.getParameterName(arguments.callee), arguments);
+			return this.AssembleProtoAuto(CMDConst.Cmd_VideoInfo, [
+				'url', 'name', 'length'
+			], arguments);
 		},
 
 		Cmd_PlaySmell: function(smell, duration, channel) {
-			return this.AssembleProtoAuto(CMDConst.Cmd_PlaySmell, this.getParameterName(arguments.callee), arguments);
+			return this.AssembleProtoAuto(CMDConst.Cmd_PlaySmell, [
+				'smell', 'duration', 'channel'
+			], arguments);
 		},
 		Cmd_StopPlay: function(channel) {
-			return this.AssembleProtoAuto(CMDConst.Cmd_StopPlay, this.getParameterName(arguments.callee), arguments);
+			return this.AssembleProtoAuto(CMDConst.Cmd_StopPlay, [
+				'channel'
+			], arguments);
 		},
 		Cmd_PlayScript: function(start, script_id) {
-			return this.AssembleProtoAuto(CMDConst.Cmd_PlayScript, this.getParameterName(arguments.callee), arguments);
+			return this.AssembleProtoAuto(CMDConst.Cmd_PlayScript, [
+				'start', 'script_id'
+			], arguments);
 		},
 		Cmd_ScriptJump: function(start) {
-			return this.AssembleProtoAuto(CMDConst.Cmd_ScriptJump, this.getParameterName(arguments.callee), arguments);
+			return this.AssembleProtoAuto(CMDConst.Cmd_ScriptJump, [
+				'start'
+			], arguments);
 		},
 		Cmd_ScriptPause: function() {
-			return this.AssembleProtoAuto(CMDConst.Cmd_ScriptPause, this.getParameterName(arguments.callee), arguments);
+			return this.AssembleProtoAuto(CMDConst.Cmd_ScriptPause);
 		},
 		Cmd_ScriptContinue: function() {
-			return this.AssembleProtoAuto(CMDConst.Cmd_ScriptContinue, this.getParameterName(arguments.callee), arguments);
+			return this.AssembleProtoAuto(CMDConst.Cmd_ScriptContinue);
 		},
 		Cmd_ScriptStop: function() {
-			return this.AssembleProtoAuto(CMDConst.Cmd_ScriptStop, this.getParameterName(arguments.callee), arguments);
+			return this.AssembleProtoAuto(CMDConst.Cmd_ScriptStop);
 		},
 	};
 
